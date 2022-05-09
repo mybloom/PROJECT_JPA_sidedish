b038615444e7201e74d14f933159a7bf4d4613d1
mybloom <baejungeun11@gmail.com>
2022-05-09 21:15:05 +0900

    init: 프로젝트 초기 셋팅

---
+++ src/main/java/com/project/sidedish_jpa/SidedishJpaApplication.java
@@ -0,0 +1,13 @@
+package com.project.sidedish_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidedishJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidedishJpaApplication.class, args);
+	}
+
+}

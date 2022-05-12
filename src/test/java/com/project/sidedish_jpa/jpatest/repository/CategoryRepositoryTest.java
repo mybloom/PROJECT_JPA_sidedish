cdbcbec40e80c31ac4faf93d0696b1cadda82e46
mybloom <baejungeun11@gmail.com>
2022-05-12 16:45:55 +0900

    [#6]fix: test class에 @ExtendWith 제거
    - SpringBoot 2.1부터는 @SpringBootTest 에 메타 주석으로 포함되어 있어 제거

--- src/test/java/com/project/sidedish_jpa/jpatest/repository/CategoryRepositoryTest.java
+++ src/test/java/com/project/sidedish_jpa/jpatest/repository/CategoryRepositoryTest.java
@@ -7,13 +7,10 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class CategoryRepositoryTest {
 

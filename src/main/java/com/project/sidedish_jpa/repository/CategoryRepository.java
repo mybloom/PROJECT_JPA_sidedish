e0c7e03f8ef515bccc90a16be77036a83f4f8983
mybloom <baejungeun11@gmail.com>
2022-05-12 11:44:06 +0900

    [#6]feat: CategoryRepository에  카테고리 목록 제공용 findAll() 작성

---
+++ src/main/java/com/project/sidedish_jpa/repository/CategoryRepository.java
@@ -0,0 +1,29 @@
+package com.project.sidedish_jpa.repository;
+
+import com.project.sidedish_jpa.domain.Category;
+import java.util.List;
+import javax.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CategoryRepository {
+
+	/**
+	 * 엔티티매니저를 왜 의존해야 하는지 작성
+	 */
+	private final EntityManager entityManager;
+
+	/**
+	 * 최상위 카테고리에 대한 하위 카테고리 목록 제공
+	 * <p>
+	 * - 모든 쿼리 필요할 때 어떻게 해야 하는지 적어두기
+	 * </p>
+	 */
+	public List<Category> findAll() {
+		return entityManager.createQuery("", Category.class)
+			.getResultList();
+	}
+
+}

079bae28812c16c3c779b1f4dbfc45cc459b1962
mybloom <baejungeun11@gmail.com>
2022-05-12 15:39:00 +0900

    [#6]feat: 최상위 카테고리에 대한 하위 카테고리 목록 제공 CategoryRepository 에 작성
    - JPQL의 fetch join 사용
    - 조인이 복잡하지 않고, 특별한 조건도 필요없으므로 fetch join 사용
    - 카테고리 조회가 fetch join를 사용할 수 있는 최적화 된 사례라고 생각한다.

--- src/main/java/com/project/sidedish_jpa/repository/CategoryRepository.java
+++ src/main/java/com/project/sidedish_jpa/repository/CategoryRepository.java
@@ -19,10 +19,13 @@ public class CategoryRepository {
 	 * 최상위 카테고리에 대한 하위 카테고리 목록 제공
 	 * <p>
 	 * - 모든 쿼리 필요할 때 어떻게 해야 하는지 적어두기
+	 * - JPQL의 페치 조인 사용
 	 * </p>
 	 */
 	public List<Category> findAll() {
-		return entityManager.createQuery("", Category.class)
+		String jpql = "select c from Category c join fetch c.parent";
+
+		return entityManager.createQuery(jpql, Category.class)
 			.getResultList();
 	}
 

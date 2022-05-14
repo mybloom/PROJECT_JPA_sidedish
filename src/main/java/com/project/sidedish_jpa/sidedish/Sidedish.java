e716c746c7025d4dfacfa3477967d9395ea648d4
mybloom <baejungeun11@gmail.com>
2022-05-14 14:43:03 +0900

    [#8]refactor: Sidedish 엔티티에 컬럼 추가 - sidedishCategories, categoryId , toString()메서드 추가

--- src/main/java/com/project/sidedish_jpa/sidedish/Sidedish.java
+++ src/main/java/com/project/sidedish_jpa/sidedish/Sidedish.java
@@ -7,8 +7,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Entity
+@Getter
+@RequiredArgsConstructor
 public class Sidedish {
 
 	@Id
@@ -27,4 +31,21 @@ public class Sidedish {
 	@OneToMany(mappedBy = "sidedish")
 	private List<SidedishImage> sidedishImages = new ArrayList<>();
 
+	@OneToMany(mappedBy = "sidedish")
+	private List<SidedishCategory> sidedishCategories = new ArrayList<>();
+
+	//이건 테이블 컬럼에서 빠져야 한다.  어떻게 해줘야 할까?
+	private Long categoryId;
+
+	@Override
+	public String toString() {
+		return "Sidedish{" +
+			"id=" + id +
+			", name='" + name + '\'' +
+			", description='" + description + '\'' +
+			", price=" + price +
+			", stockQuantity=" + stockQuantity +
+			", categoryId=" + categoryId +
+			'}';
+	}
 }
